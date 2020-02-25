b731926aac5c2f378e6339e52b3451a0eeb97f82
Lu-Ma-Ne <luguneves@gmail.com>
2020-02-25 22:12:29 +0000

    commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const greet = function(){
+    return 'Haydo!';
+};
+
+let salutation = greet();
+console.log(greet());
+
+function echo(sound){
+    return sound;
+}
+
+function greet2 (name){
+    return 'Hello' + name +'!';
+}
+
+var whereIs = function(name) {
+    return 'Where is' + name + '?';
+} ;
+
+var x = whereIs('Jacky');
+console.log(x);
+
+var hi = function(name) {
+    return 'Hi' + name +'!';
+};
+
+var h1 = hi('Selva');
+var h2 = hi('Pola');
+var x = h1 + '  ' + h2;
+console.log(x);
+
+var double = function(name) {
+    return name + ' and' + name;
+};
+var x = double('Roy'); {
+    console.log(x);
+};
+
+
