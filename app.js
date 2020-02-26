a4851ab281b5ab2736da6de08627061a300e3e6b
Lu-Ma-Ne <luguneves@gmail.com>
2020-02-26 22:50:34 +0000

    commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const greet = function(){
 var salutation = greet();
 console.log(greet(salutation));
 
-function echo(sound){
+function echo(_sound){
     return 'wowowow';
 };
 console.log(echo());
@@ -14,36 +14,64 @@ function greet2(name) {
 };
 greet2('Erika');
 
-
-
-
-
-
-/*function greet2 (name){
-    return 'Hello' + name +'!';
-}
-
 var whereIs = function(name) {
-    return 'Where is' + name + '?';
-} ;
-
+    return 'Where is' + " " +name+ " " + '?';
+};
 var x = whereIs('Jacky');
 console.log(x);
 
 var hi = function(name) {
-    return 'Hi' + name +'!';
+    return 'Hi'+" " +name+'!';
 };
 
 var h1 = hi('Selva');
 var h2 = hi('Pola');
-var x = h1 + '  ' + h2;
+var x = h1  + '  '  + h2;
 console.log(x);
 
+function shout() {
+    return 'Fire';
+};
+console.log(shout()+" " +shout());
+
 var double = function(name) {
-    return name + ' and' + name;
+    return name + " " + 'and' + " " + name;
 };
 var x = double('Roy'); {
     console.log(x);
-};*/
+};
+
+function doSomething(_name) {
+return 'Lucia';
+};
+console.log(doSomething());
+
+function multiplyFive (number) {
+    return number * 5;
+};
+console.log(multiplyFive(4));
+
+function myFunction(alert) {
+    alert = 'Hello' + " " + 'World!';
+   return alert;
+};
+console.log(myFunction());
+
+var isAnswerRight = true;
+if (isAnswerRight) {
+    console.log('The answer is right');
+} else {
+    console.log('The answer is wrong');
+}
+
+isAnswerRight ? console.log('The answer is wrong') : console.log('The answer is right');
+
+
+
+
+
+
+
+
 
 
